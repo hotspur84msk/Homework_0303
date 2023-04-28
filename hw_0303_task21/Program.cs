f3142295057f0da203897e1123d5ece591d92138
Sergei.Petrushin <sjabrail@gmail.com>
2023-04-28 16:21:42 +0530

    program is done

--- hw_0303_task21/Program.cs
+++ hw_0303_task21/Program.cs
@@ -3,3 +3,18 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.WriteLine("Введите координаты для точки А: ");
+
+double ax = Convert.ToInt32(Console.ReadLine());
+double ay = Convert.ToInt32(Console.ReadLine());
+double az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты для точки В: ");
+
+double bx = Convert.ToInt32(Console.ReadLine());
+double by = Convert.ToInt32(Console.ReadLine());
+double bz = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow((ax-bx), 2) + Math.Pow((ay-by), 2) + Math.Pow((az-bz), 2));
+
+Console.WriteLine($"Расстояние между А и В = {Math.Round(distance, 3)}");
