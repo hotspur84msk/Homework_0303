4b44bd0dba9bcead31b52995b84c0aae48601bc3
Sergei.Petrushin <sjabrail@gmail.com>
2023-04-28 17:47:02 +0530

    program is done

--- hw_0303_task23/Program.cs
+++ hw_0303_task23/Program.cs
@@ -3,3 +3,15 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.Write("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+double result = 1;
+int i = 1;
+
+while(i <= N)
+{
+    result = Math.Pow(i, 3);
+    Console.Write($"{result} ");
+    i++;
+}
